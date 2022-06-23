f3941402137eb1bcbf9f72ec26c5538f3e311c78
Naman Shah <65423813+naman0301@users.noreply.github.com>
2022-06-22 20:37:04 -0400

    Default and null update in form.sql

--- Form.sql
+++ Form.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `Form` (
-  `FULLNAME` varchar(25) DEFAULT NULL,
-  `EMAIL` varchar(25) NOT NULL DEFAULT '',
-  `ROOMS` decimal(10,0) DEFAULT NULL,
-  `ADULTS` decimal(10,0) DEFAULT NULL,
-  `CHILDRENS` decimal(10,0) DEFAULT NULL,
+  `FULLNAME` varchar(25) NOT NULL,
+  `EMAIL` varchar(25) NOT NULL,
+  `ROOMS` decimal(10,0) NOT NULL,
+  `ADULTS` decimal(10,0) NOT NULL,
+  `CHILDRENS` decimal(10,0) NOT NULL,
   PRIMARY KEY (`EMAIL`)
 )
