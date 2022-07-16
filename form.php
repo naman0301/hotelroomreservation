9b3f629b88e757182b06e3ea2ab0280ae68c4c2c
Naman Shah <65423813+naman0301@users.noreply.github.com>
2022-07-16 15:37:09 -0400

    Change of database using PhpMyAdmin and webhost
    This commit consists of some major changes in the PHP file. The hosting as well as data storing platform has been shifted from the Humber College server to the WebHost server.
    
    This commit also includes code that saves check-in and checkout dates taken from the user's input into the MySQL database

--- form.php
+++ form.php
@@ -1,18 +1,18 @@
 <?php
 	$name = $_REQUEST['name'];
 	$email= $_REQUEST['email'];
-	$checkin = $_REQUEST['checkin'];
-	printf("<b>Check-in Date:</b> %s || ",$checkin);
-	$checkout = $_REQUEST['checkout'];
-	printf("<b>Check-out Date:</b> %s || ",$checkout);
+	$checkin = date('Y-m-d', strtotime($_REQUEST['checkin']));
+	$checkout = date('Y-m-d', strtotime($_REQUEST['checkout']));
 	$rooms= $_REQUEST['number'];
 	$adults= $_REQUEST['number1'];
 	$childrens= $_REQUEST['number2'];
 	
+	// Authentic details are hidden due to security reasons 
+
 	$servername = "localhost";
-	$username = "n01392496";
-	$database = "n01392496_a";
-	$password = "n01392496";
+	$username = "********";
+	$database = "**********";
+	$password = "********";
 
 	$conn = mysqli_connect($servername,$username,$password,$database);
 
@@ -20,27 +20,28 @@
 	   die("Connection failed: " . mysqli_connect_error());
 	}
 
-$sql = "insert into Form(FULLNAME,EMAIL,ROOMS,ADULTS,CHILDRENS)
-				values('$name','$email','$rooms','$adults','$childrens')";
+$sql = "insert into Form(FULLNAME,EMAIL,ROOMS,ADULTS,CHILDRENS,CHECKIN,CHECKOUT)
+				values('$name','$email','$rooms','$adults','$childrens','$checkin','$checkout')";
 	// echo "<br>SQL generated: $sql" . "<br>\n";
 
 	if(mysqli_query($conn, $sql)){
-	   echo "\nRecord created";
+	   echo "\nRecord created, Booking Confirm!!";
 	}else{
-	   echo "\nFailure to create record" . mysqli_error($conn);
+	   echo "\nFailure to create record, " . mysqli_error($conn);
 	}
+
 $sql1 = "select * from Form";
 
 	$result = mysqli_query($conn,$sql1);
 
 echo "<h1>Form DataBase</h1>";
 		echo "<table border = 1>";
-		$heading = "<tr><th>FULLNAME</th><th>EMAIL</th><th>ROOMS</th><th>ADULTS</th><th>CHILDRENS</th></tr>";
+		$heading = "<tr><th>FULLNAME</th><th>EMAIL</th><th>ROOMS</th><th>ADULTS</th><th>CHILDRENS</th><th>CHECKIN</th><th>CHECKOUT</th></tr>";
 		echo $heading;
 	if(mysqli_num_rows($result) > 0){
 
 		while($row = mysqli_fetch_assoc($result)){
-			$data = sprintf("<tr><td>%s</td><td>%s</td><td>%d</td><td>%d</td><td>%d</td></tr>\n",$row['FULLNAME'],$row['EMAIL'],$row['ROOMS'],$row['ADULTS'],$row['CHILDRENS']);
+			$data = sprintf("<tr><td>%s</td><td>%s</td><td>%d</td><td>%d</td><td>%d</td><td>%s</td><td>%s</td></tr>\n",$row['FULLNAME'],$row['EMAIL'],$row['ROOMS'],$row['ADULTS'],$row['CHILDRENS'],$row['CHECKIN'],$row['CHECKOUT']);
 			echo $data;
 		}	
 		echo "</table>";
@@ -49,7 +50,6 @@ echo "<h1>Form DataBase</h1>";
 	}
 
 	echo "<br>\n";
-
 	mysqli_close($conn);
 ?>
-<h3> Booking Confirmed</h3>
+
